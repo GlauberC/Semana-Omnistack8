30651a87467280f5488f350cf3d0ec7ee13cfae7
GlauberC <glaubercarv@gmail.com>
2019-08-09 13:42:44 -0300

    node

---
+++ backend/src/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const routes = require('./routes')
+const mongoose = require('mongoose')
+
+const server = express()
+
+server.use(express.json())
+server.use(routes)
+
+mongoose.connect( "mongodb+srv://omnistack:omnistack@cluster0-le69g.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+
+
+server.listen(3333)
+
+
+
+
+
+
+// server.get('/', (req, res) => {
+//     return res.json({message: `Hello ${req.query.name}`})
+// })
+
+
+// server.get('/', (req, res) => {
+//     return res.send(`Olá ${req.query.name}`)
+//     // http://localhost:3333/?name=Glauber
+// })
+
+// server.listen(3333)
