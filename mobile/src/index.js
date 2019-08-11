448e7a327137d26df6907efc910a5b901be6c32a
GlauberC <glaubercarv@gmail.com>
2019-08-11 11:38:15 -0300

    react native sem expo

---
+++ mobile/src/index.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>Semana Omnistack 8</Text>
+    </View>
+  );
+};
+
+
+export default App;
